e388e804106f462aff74952fbdbfc6183f9a760a
pavel <ponch092009@yandex.ru>
2022-10-03 21:45:00 +0300

    add

--- main.py
+++ main.py
@@ -11,23 +11,27 @@ import PySAM.Cashloan as Cashloan
 import PySAM.Singleowner as Single
 
 pv = PVWatts.new()
+batt = Batt.from_existing(pv)
+fuelcell = Fuel.from_existing(pv)
 grid = Grid.from_existing(pv)
+sl = Single.from_existing(pv)
 ur = UtilityRate.from_existing(pv)
-cl = ThermalRate.from_existing(pv)
-
+tl = ThermalRate.from_existing(pv)
 
 if __name__ == '__main__':
 
-    jsons_path = "/home/pavel/Документы/py/jsons/"
+    jsons_path = "./jsons/"
     file_names = [f for f in os.listdir(jsons_path)
                   if os.path.isfile(os.path.join(jsons_path, f))]
     print(file_names)
-    file_names = ["untitled__1__pvwattsv8.json", "untitled__1__grid.json", "untitled__1__utilityrate5.json", "untitled__1__thermalrate.json"]
-    modules = [pv, grid, ur, cl]
+    file_names = ['untitled__1__pvwattsv8.json',  'untitled__1__utilityrate5.json', 'untitled__1__singleowner.json']
+    print(file_names)
+    # , ,
+    modules = [pv, ur, sl]
 
     for f, m in zip(file_names, modules):
 
-        with open(jsons_path+ f , 'r') as file:
+        with open(jsons_path + f, 'r') as file:
             data = json.load(file)
             for k, v in data.items():
                 if k != "number_inputs":
@@ -36,3 +40,6 @@ if __name__ == '__main__':
     for m in modules:
         m.execute()
         print('ex')
+print('ac_annual: ', pv.Outputs.ac_annual)
+print('ur_ec_tou_mat: ', ur.ElectricityRates.ur_ec_tou_mat)
+#print('cl.Outputs.npv: ', tl.Outputs.npv)
---
+++ tst.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  3 09:02:22 2020
+
+This is an example script showing how to use data exported from a SAM PVWatts
+Distributed Commercial simulation and do the same simulation in PySAM.
+
+The steps are:
+    Find the modules needed for your simulation type in the docs.
+    Load json file with inputs from SAM GUI.
+    Load all needed PySAM module's data structures with the json input data.
+    Run the simulations.
+    Print the results.
+
+Most recently tested against PySAM 3.0.2
+
+@author: frohro
+"""
+
+import json
+import PySAM.Pvwattsv8 as PVWatts
+import PySAM.Grid as Grid
+import PySAM.Utilityrate5 as UtilityRate
+import PySAM.Cashloan as Cashloan
+import PySAM.PySSC as pssc
+
+ssc = pssc.PySSC()
+
+verbose = True  # Make False if you don't want all the debugging info.
+
+
+# Get the SAM json file, make the simulations we need for the commercial
+# PVWatts simulation.
+
+# The json file is generated from SAM using the "Generate Code" menu item
+# in the added simulation case.  Choose "JSON for inputs" and a .json file
+# with the title of your simulation case will be created where you select with
+# the "Open" button on the file dialog.
+json_file_path = 'jsons/100kW_PVWatts.json'  # Change this file name to yours!
+with open(json_file_path) as f:
+        dic = json.load(f)
+# The next seven lines are needed to load the PySAM data structures with the
+# inputs from the json file.
+pv_dat = pssc.dict_to_ssc_table(dic, "pvwattsv8")
+grid_dat = pssc.dict_to_ssc_table(dic, "grid")
+ur_dat = pssc.dict_to_ssc_table(dic, "utilityrate5")
+cl_dat = pssc.dict_to_ssc_table(dic, "cashloan")
+pv = PVWatts.wrap(pv_dat)
+grid = Grid.from_existing(pv)
+ur = UtilityRate.from_existing(pv)
+cl = Cashloan.from_existing(pv)
+grid.assign(Grid.wrap(grid_dat).export())
+ur.assign(UtilityRate.wrap(ur_dat).export())
+cl.assign(Cashloan.wrap(cl_dat).export())
+
+# The models are executed in order.  Note that the outputs from the first
+# simulation are automatically available for the next one, and so on.  :-)
+pv.execute()
+grid.execute()
+ur.execute()
+cl.execute()
+
+if verbose:  # Print out some results.  The variable names can be found in
+    # the model pages of the docs.
+    print('Replicating SAM Results using PySAM')
+    print('ac_annual: ', pv.Outputs.ac_annual)
+    print('annual_energy: ', pv.Outputs.annual_energy)
+    print('ur_ec_tou_mat: ', ur.ElectricityRates.ur_ec_tou_mat)
+    print('cl.Outputs.npv: ', cl.Outputs.npv)
+    print('cl.FinancialParameters.analysis_period: ',
+          cl.FinancialParameters.analysis_period)
+    print('cl.FinancialParameters.real_discount_rate: ',
+          cl.FinancialParameters.real_discount_rate)
+    print('cl.Outputs.adjusted_installed cost',
+          cl.Outputs.adjusted_installed_cost)
+    print(pv.Outputs.ac_monthly)
